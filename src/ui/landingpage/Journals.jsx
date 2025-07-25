561f0dffcd90f3711de1036d6e3ee5696f788d66
Ahmed Ayinla <aayprince@gmail.com>
2025-07-25 19:58:07 +0100

    merged error on Journals

--- src/ui/landingpage/Journals.jsx
+++ src/ui/landingpage/Journals.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import Button from "@/components/ui/Button";
-
-const Journals = () => {
-  const arr = Array(3).fill(null);
-  return (
-    <div className="bg-no-repeat bg-cover bg-center bg-[url('/images/journal-bg.png')] relative p-5 py-8">
-      <article className="flex flex-col items-center relative gap-15 z-10">
-        <h2 className="font-extrabold text-(--background) text-4xl">
-          Latest Publications
-        </h2>
-        <div className="flex items-center justify-center gap-15">
-          {arr.map((_, i) => {
-            return <img key={i} src="/images/journal.png" alt="Journal" />;
-          })}
-        </div>
-
-        <Button variant="tertiary">View all Publications</Button>
-      </article>
-      <div className="bg-linear-90 to-[#fef103b2] from-0% from-[#03823a] to-100% absolute inset-0" />
-=======
 // src/ui/landingpage/Journals.jsx
 
 import Slider from "react-slick";
@@ -144,14 +123,11 @@ const Journals = () => {
       </article>
 
       <div className="absolute inset-0 bg-gradient-to-r from-[#03823a] to-[#fef103b2] opacity-80 z-0" />
->>>>>>> dev
     </div>
   );
 };
 
 export default Journals;
-<<<<<<< HEAD
-=======
 
 
 
@@ -186,4 +162,3 @@ export default Journals;
 // };
 
 // export default Journals;
->>>>>>> dev
