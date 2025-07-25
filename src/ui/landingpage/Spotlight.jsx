bb75b1ccb07f3dc313efd8888aec05caee24728e
Ahmed Ayinla <aayprince@gmail.com>
2025-07-25 19:41:47 +0100

    merged error on Spotlight file

--- src/ui/landingpage/Spotlight.jsx
+++ src/ui/landingpage/Spotlight.jsx
@@ -6,7 +6,6 @@ const spotlightData = [
     title: "Iconic Infrastructure of the Year",
     category: "Spotlight",
     description:
-<<<<<<< HEAD
       "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.",
     image: "/images/news.png",
   },
@@ -21,9 +20,6 @@ const spotlightData = [
     title: "Young Civil Engineer of the Month",
     category: "Recognitions",
     description:
-      "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.",
-    image: "/images/news.png",
-=======
       "Celebrating the most innovative and impactful infrastructure project that advanced engineering excellence and public good.",
     image: "/images/spotlight1.png",
   },
@@ -40,43 +36,27 @@ const spotlightData = [
     description:
       "Recognizing a rising star under 35 who has demonstrated excellence in civil engineering and community impact.",
     image: "/images/spotlight3.jpg",
->>>>>>> dev
   },
   {
     title: "Best Chapter of the Year",
     category: "Group Dynamics",
     description:
-<<<<<<< HEAD
-      "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.",
-    image: "/images/news.png",
-=======
       "Awarded to the most vibrant and impactful NICE chapter that exemplified professional development and member engagement.",
     image: "/images/spotlight4.jpg",
->>>>>>> dev
   },
   {
     title: "Corporate Partner of the Year",
     category: "Awards",
     description:
-<<<<<<< HEAD
-      "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.",
-    image: "/images/news.png",
-=======
       "Acknowledging a corporate organization whose support significantly advanced NICE’s mission and engineering practice.",
     image: "/images/spotlight5.png",
->>>>>>> dev
   },
   {
     title: "Best Chapter Award",
     category: "Awards",
     description:
-<<<<<<< HEAD
-      "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.",
-    image: "/images/news.png",
-=======
       "Honoring the NICE chapter that has consistently demonstrated operational excellence, innovation, and community service.",
     image: "/images/spotlight6.png",
->>>>>>> dev
   },
 ];
 
@@ -88,11 +68,7 @@ const FeaturedSpotlights = () => {
           Featured Spotlights
         </h2>
 
-<<<<<<< HEAD
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-=======
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
->>>>>>> dev
           {spotlightData.map((item, index) => (
             <SpotlightCard item={item} key={index} />
           ))}
